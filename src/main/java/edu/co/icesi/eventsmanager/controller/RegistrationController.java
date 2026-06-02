013b324538994a1dcafa534bd560c726adede94a
Ismael Barrionuevo <98758380+DavidRamirez206@users.noreply.github.com>
2026-06-01 22:06:34 -0500

    Refactor myRegistrations method to use repository

--- src/main/java/edu/co/icesi/eventsmanager/controller/RegistrationController.java
+++ src/main/java/edu/co/icesi/eventsmanager/controller/RegistrationController.java
@@ -1,7 +1,10 @@
 package edu.co.icesi.eventsmanager.controller;
 
+import edu.co.icesi.eventsmanager.document.EventRegistration;
 import edu.co.icesi.eventsmanager.security.CustomUserDetails;
 import edu.co.icesi.eventsmanager.service.RegistrationService;
+import edu.co.icesi.eventsmanager.repository.EventRegistrationRepository;
+import edu.co.icesi.eventsmanager.repository.EventStatisticRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,13 +16,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import edu.co.icesi.eventsmanager.entity.EventStatistic;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.List;
 
 @Controller
 public class RegistrationController {
 
     @Autowired
+    private EventRegistrationRepository registrationRepository;
+
+    @Autowired
     private RegistrationService registrationService;
+
+    @Autowired
+    private EventStatisticRepository eventStatisticRepository;
 
     @GetMapping("/my-registrations")
     @PreAuthorize("hasAnyRole('STUDENT', 'EMPLOYEE')")
@@ -33,8 +45,8 @@ public class RegistrationController {
             .filter(stat -> stat != null)
             .collect(Collectors.toMap(EventStatistic::getEventId, EventStatistic::getEventTitle, (existing, replacement) -> existing));
 
-        model.addAttribute("registrations", data.get("registrations"));
-        model.addAttribute("eventTitles", data.get("eventTitles"));
+        model.addAttribute("registrations", registrations);
+        model.addAttribute("eventTitles", eventTitles);
         return "my_registrations";
     }
 
